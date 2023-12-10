3ba770f4aca7002a8a739aa7d66500170a4cd707
saeed.da1994 <saeed.da1994@gmail.com>
2023-12-10 23:22:41 +0330

    - adding sticky nav using toggle in js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-console.log('hello from personal portfolio')
+
+//scroll section for sticky header
+window.onscroll = () => {
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}

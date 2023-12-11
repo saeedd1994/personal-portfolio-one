e9a3b9fe364b48f35a7cd13eea14a0d750347c4d
saeed.da1994 <saeed.da1994@gmail.com>
2023-12-11 22:32:14 +0330

    - adding active class to each section

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,26 @@
+//scroll section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-//scroll section for sticky header
+
 window.onscroll = () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 100;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            //     active navbar
+            navLinks.forEach(link =>{
+                link.classList.remove('active');
+                document.querySelector(`header nav a[href*='${id}']`).classList.add('active');
+            })
+        }
+
+    })
+
+    // sticky nav header
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 }
